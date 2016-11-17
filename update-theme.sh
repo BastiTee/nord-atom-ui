bfd5c6456e146cbd7fd973e97ee58e8552b32ab2
Basti Tee <basti.tee@gmx.de>
2016-11-17 11:41:28 +0100

    added color and size changes;

---
+++ update-theme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+atom=/usr/share/atom/atom
+ps -ef | grep $atom | grep -v grep | awk '{print $2}' | while read pid; do kill -9 $pid; done 2>&1 > /dev/null
+apm unlink
+apm install
+apm link
+$atom -d 2>&1 > /dev/null & 
+echo
+
